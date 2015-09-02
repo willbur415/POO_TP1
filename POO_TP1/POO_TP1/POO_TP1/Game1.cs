be7762924ac341470711f56c7d71f561f209ee65
William Déry <dery.william@hotmail.com>
2015-09-02 10:11:45 -0400

    Changement de namespace

--- POO_TP1/POO_TP1/POO_TP1/Game1.cs
+++ POO_TP1/POO_TP1/POO_TP1/Game1.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using Exercice5;
+using POO_TP1;
 
 namespace POO_TP1
 {
