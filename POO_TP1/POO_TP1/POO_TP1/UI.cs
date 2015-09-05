681cb99500d7d368f797d7dd7ddc93a653e82f0f
William Déry <dery.william@hotmail.com>
2015-09-05 12:39:10 -0400

    Added ui class and PlayerShipLife sprite

---
+++ POO_TP1/POO_TP1/POO_TP1/UI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace POO_TP1
+{
+    class UI
+    {
+        private static UI ui;
+        private int numberOfLife;
+        private int score;
+
+        public static UI GetInstance()
+        {
+            if (ui == null)
+            {
+                ui = new UI();
+            }
+            return ui;
+        }
+
+        public void Initialize()
+        {
+            this.numberOfLife = 3;
+            this.score = 3;
+        }
+
+        
+    }
+}
