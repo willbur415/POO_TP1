be04c71b19aa9e9c3c88c8b219ea16326faae628
Eric Boudreau <cudajam@gmail.com>
2015-09-07 17:21:20 -0400

    saving to xml now works

--- POO_TP1/POO_TP1/POO_TP1/Scores.cs
+++ POO_TP1/POO_TP1/POO_TP1/Scores.cs
@@ -30,7 +30,6 @@ namespace POO_TP1
         {
             this.graphics = graphics;
             this.font = font;
-
         }
 
         public void ShowScores(Dictionary<string,string> scoreList, ref SpriteBatch spriteBatch )
@@ -48,27 +47,27 @@ namespace POO_TP1
                                         Color.White, 0, origin, 1.0f, SpriteEffects.None, 0.5f);
                 posCounter += 75;
             }
-            
-            
+                  
         }
 
         public void saveToXML(string score, string name)
         {
             XmlDocument doc = new XmlDocument();
-            doc.Load(@"..\..\res\XML\scorelog.xml");
+            doc.Load(@"..\..\..\..\POO_TP1Content\scorelog.xml");
+            XmlNode assetNode = doc.SelectSingleNode("XnaContent/Asset");
 
-            XmlNode entryNode = doc.CreateNode(XmlNodeType.Element, "Log", "");
-            doc.DocumentElement.AppendChild(entryNode);
+            XmlNode entryNode = doc.CreateNode(XmlNodeType.Element, "Item", "");
+            assetNode.AppendChild(entryNode);
 
-            XmlNode entryNameNode = doc.CreateNode(XmlNodeType.Element, "Player", "");
+            XmlNode entryNameNode = doc.CreateNode(XmlNodeType.Element, "Key", "");
             entryNameNode.InnerText = name;
             entryNode.AppendChild(entryNameNode);
 
-            XmlNode entryScoreNode = doc.CreateNode(XmlNodeType.Element, "Score", "");
+            XmlNode entryScoreNode = doc.CreateNode(XmlNodeType.Element, "Value", "");
             entryScoreNode.InnerText = score;
             entryNode.AppendChild(entryScoreNode);
 
-            doc.Save(@"..\..\res\XML\scorelog.xml");
+            doc.Save(@"..\..\..\..\POO_TP1Content\scorelog.xml");
         }
 
         public SpriteFont Font
