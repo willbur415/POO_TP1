598816ae8a645afa3a9ed637442a7e85eae86f3d
William Déry <dery.william@hotmail.com>
2015-09-07 23:06:33 -0400

    Changed Bonus so they could work

---
+++ POO_TP1/POO_TP1/POO_TP1/Subjects/BonusType/DoublePoints.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace POO_TP1.Subjects.BonusType
+{
+    public class DoublePoints : Bonus
+    {
+        private const int SCORE_MULTIPLIER = 2;
+        private const int BONUS_DURATION = 150;
+
+        public DoublePoints(Texture2D image, Vector2 position)
+            : base(image, position)
+        {
+            
+        }
+
+        public override void StartEffect()
+        {
+            UI.GetInstance().ScoreMultiplier = SCORE_MULTIPLIER;
+            base.bonusTime = BONUS_DURATION;
+        }
+
+        public override void Update()
+        {
+            if (bonusTime > 0)
+            {
+                bonusTime--;
+            }
+            else
+            {
+                this.NotifyAllObservers();
+            }
+        }
+    }
+}
